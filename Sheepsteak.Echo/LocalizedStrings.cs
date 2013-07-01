1cb28a01e55968308696133a871f9eb5a6d69540
Chris Shepherd <chris@chrisshepherd.me>
2013-07-01 22:31:22 +0100

    Initial commit

---
+++ Sheepsteak.Echo/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Sheepsteak.Echo.Resources;
+
+namespace Sheepsteak.Echo
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
